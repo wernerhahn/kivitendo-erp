0be80dd099bce7d0c36327519e2cd1a5a65756d5
G. Richardson <information@kivitendo-premium.de>
2015-09-24 02:24:37 +0200

    Shop - first attempt at determining updated parts

--- SL/Shop.pm
+++ SL/Shop.pm
@@ -4,6 +4,7 @@ use strict;
 
 use parent qw(Rose::Object);
 use SL::ShopConnector::ALL;
+use SL::DB::Part;
 
 # __PACKAGE__->run_before('check_auth');
 
@@ -12,10 +13,39 @@ use Rose::Object::MakeMethods::Generic (
   'scalar --get_set_init' => [ qw(connector) ],
 );
 
+sub updatable_parts {
+  my ($self, $last_update) = @_;
+  $last_update ||= DateTime->now(); # need exact timestamp, with minutes
+
+  my $parts;
+  my $active_shops = SL::DB::Manager::Shop->get_all(query => [ obsolete => 0 ]);
+  foreach my $shop ( @{ $active_shops } ) {
+    # maybe run as an iterator? does that make sense with with_objects?
+    my $update_parts = SL::DB::Manager::ShopPart->get_all(query => [
+             and => [
+                'active' => 1,
+                'shop_id' => $shop->id,
+                # shop => '1',
+                or => [ 'part.mtime' => { ge => $last_update },
+                        'part.itime' => { ge => $last_update },
+                        'itime'      => { ge => $last_update },
+                        'mtime'      => { ge => $last_update },
+                      ],
+                    ]
+             ],
+             with_objects => ['shop', 'part'],
+             # multi_many_ok   => 1,
+          );
+    push( @{ $parts }, @{ $update_parts });
+  };
+  return $parts;
+
+};
+
 sub init_connector {
   my ($self) = @_;
   # determine the connector from the connector type in the webshop config
-  return SL::ShopConnector::ALL->shop_connector_class_by_name($self->config->connector)->new( config => $self->config); 
+  return SL::ShopConnector::ALL->shop_connector_class_by_name($self->config->connector)->new( config => $self->config);
 
 };
 
@@ -27,11 +57,11 @@ __END__
 
 =head1 NAME
 
-SL::WebShop - Do stuff with WebShop instances
+SL::Shop - Do stuff with Shop instances
 
 =head1 SYNOPSIS
 
-  my $config = SL::DB::Manager::WebShop->get_first();
+  my $config = SL::DB::Manager::Shop->get_first();
   my $shop = SL::WebShop->new( config => $config );
 
 From the config we know which Connector class to load, save in $shop->connector
